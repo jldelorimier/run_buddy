ac5fa1568309bd87773c46c43ab5017ac22a9cee
Julia de Lorimier <jldelorimier@gmail.com>
2023-08-18 16:48:26 -1000

    adds new PagesController and index action to PagesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   mount Sidekiq::Web => "/sidekiq" if defined?(Sidekiq) # monitoring console
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
