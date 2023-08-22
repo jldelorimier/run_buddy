23d633750dad4541eda28f6278fb80a9fc58d342
Julia de Lorimier <jldelorimier@gmail.com>
2023-08-21 15:50:02 -1000

    Adds ability to sign out on top of Devise authentication; updates root route to pages#index; fixes typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'pages#index'
   get 'pages/index'
   mount Sidekiq::Web => "/sidekiq" if defined?(Sidekiq) # monitoring console
-  root "home#index"
+  # root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
