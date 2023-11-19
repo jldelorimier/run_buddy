224b1c12e9cfb72ff9a353aef233bc72fb4b89fa
Julia de Lorimier <jldelorimier@gmail.com>
2023-11-19 18:07:14 -0500

    Attempts to display list of matches on the index page for a given user... this isn't working. Attempts to add association from Users to Matches by joining on Runs

--- app/models/match.rb
+++ app/models/match.rb
@@ -11,6 +11,7 @@
 class Match < ApplicationRecord
   belongs_to :run1, class_name: 'Run', foreign_key: 'run1'
   belongs_to :run2, class_name: 'Run', foreign_key: 'run2'
+  # has_many :users, through: :runs
 
   validates :run1, uniqueness: { scope: :run2 }
   validates :run2, uniqueness: { scope: :run1 }
