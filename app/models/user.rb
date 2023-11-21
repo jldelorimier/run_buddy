76443dfb133a562c80e179674e2cb934d01989aa
Julia de Lorimier <jldelorimier@gmail.com>
2023-11-20 20:50:07 -0500

    Edits the SQL query in User model to query for user_ids on matches in a different way... index view still isn't finding any matches on current_user. i.e. current_user.matches.present? is returning false for user1

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,16 +24,16 @@ class User < ApplicationRecord
       # V1
       # .where('run1.user = ? OR run2.user = ?', user.id, user.id) 
       # V2
-      # .joins('INNER JOIN runs AS run1 ON run1.id = matches.run1')
-      # .joins('INNER JOIN runs AS run2 ON run2.id = matches.run2')
-      # .joins('INNER JOIN users AS user1 ON user1.id = run1.user')
-      # .joins('INNER JOIN users AS user2 ON user2.id = run2.user')
-      # .where('user1.id = ? OR user2.id = ?', user.id, user.id)
+      .joins('INNER JOIN runs AS run1 ON run1.id = matches.run1')
+      .joins('INNER JOIN runs AS run2 ON run2.id = matches.run2')
+      .joins('INNER JOIN users AS user1 ON user1.id = run1.user_id')
+      .joins('INNER JOIN users AS user2 ON user2.id = run2.user_id')
+      .where('user1.id = ? OR user2.id = ?', user.id, user.id)
       # V3
-      .joins(run1: :user, run2: :user)
+      # .joins(run1: :user, run2: :user)
         # .where('users.id = ? OR users.id = ?', user.id, user.id)
-        .where('runs.user_id = :user_id OR runs.user_id = :user_id', user_id: user.id)
-    }, class_name: 'Match', foreign_key: 'run1.user_id'
+        # .where('run1.user_id = :user_id OR run2.user_id = :user_id', user_id: user.id) # `run1.user_id` and `run2.user_id` are giving me an ActiveRecord error
+    }, class_name: 'Match', foreign_key: 'run1' #Chad says to change to foreign_key to 'run1_user_id'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
